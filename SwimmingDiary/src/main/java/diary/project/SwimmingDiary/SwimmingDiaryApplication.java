31b17bc7c22b6c5bff6e6ca1cee00dff4dd31bcb
Dorijan <dorijan.grgic@fer.hr>
2019-07-13 20:32:23 +0200

    Core structure added

---
+++ SwimmingDiary/src/main/java/diary/project/SwimmingDiary/SwimmingDiaryApplication.java
@@ -0,0 +1,13 @@
+package diary.project.SwimmingDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwimmingDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwimmingDiaryApplication.class, args);
+	}
+
+}
---
+++ SwimmingDiary/src/test/java/diary/project/SwimmingDiary/SwimmingDiaryApplicationTests.java
@@ -0,0 +1,16 @@
+package diary.project.SwimmingDiary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SwimmingDiaryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
